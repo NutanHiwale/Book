b83eff96e37d49f0a4c3b65c024ce2a67e7ac7f1
Nutan Hiwale <nutan.hiwale@joshsoftware.com>
2022-01-25 18:14:18 +0530

    Added Initital Models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    def xyz
+        byebug
+        id 
+    end
 end
--- db/migrate/20220124100030_create_books.rb
+++ db/migrate/20220124100030_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.integer :author_id 
+      t.string :title
+      t.string :isbn 
       t.timestamps
     end
   end
